0f0b3ed25bd96f1813090b4664f9c18c294a8953
Hoàng Văn Hiếu <vanhieu290101@gmail.com>
2023-11-21 19:38:11 +0700

    update header

--- front-end/my-app/src/pages/user/theme/header/index.js
+++ front-end/my-app/src/pages/user/theme/header/index.js
@@ -1,8 +1,48 @@
 import {memo} from 'react';
-
+import "./style.scss";
+import { AiOutlineFacebook, AiOutlineGlobal, AiOutlineInstagram, AiOutlineLinkedin,AiOutlineUser } from "react-icons/ai";
+import { Link } from "react-router-dom";
 const Header = () => {
     return (
-    <h1>Header</h1>
+    <div className="header__top">
+        <div className='container'>
+            <div className="row">
+                <div className="col-6"> Left </div>
+                <div className="col-6 header__top_right">
+                    <ul>
+                        <li>
+                            <Link to={""}>
+                            <AiOutlineFacebook />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                            <AiOutlineInstagram />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                            <AiOutlineLinkedin />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                            <AiOutlineGlobal />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                            <AiOutlineUser />
+                            </Link>
+                        </li>
+                        <span>
+                            Đăng nhập
+                        </span>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
     );
 }
 
