124973c0f72ffd3b39923ca0e6565cff1e01e692
Hoàng Văn Hiếu <vanhieu290101@gmail.com>
2023-11-21 21:45:04 +0700

    add responsive

--- front-end/my-app/src/pages/user/theme/header/index.js
+++ front-end/my-app/src/pages/user/theme/header/index.js
@@ -1,13 +1,25 @@
 import {memo} from 'react';
 import "./style.scss";
-import { AiOutlineFacebook, AiOutlineGlobal, AiOutlineInstagram, AiOutlineLinkedin,AiOutlineUser } from "react-icons/ai";
+import { AiOutlineFacebook, AiOutlineGlobal, AiOutlineInstagram, AiOutlineLinkedin,AiOutlineUser,AiOutlineMail } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { formatPrice } from "utils/formater"
 const Header = () => {
     return (
-    <div className="header__top">
+    <div>
+        <div className="header__top">
         <div className='container'>
             <div className="row">
-                <div className="col-6"> Left </div>
+                <div className="col-6 header__top_left">
+                    <ul>
+                        <li>
+                            <AiOutlineMail />
+                            bku-shop@hcmut.edu.vn
+                        </li>
+                        <li>
+                            Miễn phí giao hàng cho đơn hàng từ {formatPrice(200000)}
+                        </li>
+                    </ul> 
+                </div>
                 <div className="col-6 header__top_right">
                     <ul>
                         <li>
@@ -34,11 +46,26 @@ const Header = () => {
                             <Link to={""}>
                             <AiOutlineUser />
                             </Link>
-                        </li>
-                        <span>
+                            <span>
                             Đăng nhập
                         </span>
+                        </li>
+                        
                     </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-xl-3">
+                    LOGO
+                </div>
+                <div className="col-xl-6">
+                    MENU
+                </div>
+                <div className="col-xl-3">
+                    PHONE
                 </div>
             </div>
         </div>
